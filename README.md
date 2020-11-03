32f8ead7c441058bb4fb518f7d8ec341de6bff3e
Choi702 <jaeychoi1@gmail.com>
2020-11-03 09:55:28 -0800

    update app.sj and worked on questions to correct bugs

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # demo-day-2
-Some css, Js and git basic
+Interactive questions that we created in class
 
+# Author #
+Jae Choi
+
+
+Some css, Js and git basic
+revised the questions to make it more fun
 Added in Question 4 and 5
 Added in Question 6 and 7
 Added console.log
 
-
-Corrected all my issue for lab1 - lab4
-
-Driver was Klace
-Navigator was Jae
+## Contributor ##
+Paired Programming
+Driver was Klace Koch
+Navigator was Jae Choi
 
 We switched roles
-Driver was me
-Navigator was Klace
+Driver was me Jae Choi
+Navigator was Klace Koch
 
 We were both able to get the functions working following each others instructions
+
--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 /* 
   guessing game about me
 */
+var totalCorrect = 0;
+var totalIncorrect = 0;
 
 var whatIsYourName = 'no name'
 console.log('Start of my page')
@@ -14,9 +16,10 @@ function questionOne() {
   var letsPlayAGame = prompt('Do you want to ask some questions about Jae? (y/n)');
   if (letsPlayAGame === 'y' || letsPlayAGame === 'Y' || letsPlayAGame === 'YES' || letsPlayAGame === 'Yes' || letsPlayAGame === 'yes') {
     alert(whatIsYourName + ' YOU ARE A SCHOLAR!!' + ' Lets start shall we!!');
-
+    totalCorrect++;
   } else if (letsPlayAGame === 'n' || letsPlayAGame === 'N' || letsPlayAGame === 'NO' || letsPlayAGame === 'No' || letsPlayAGame === 'no') {
     alert(whatIsYourName + ' BOOO!!!, you should really ask him some questions, he might amaze you!!');
+    totalIncorrect++;
   }
   console.log('finished the first function')
 }
@@ -27,10 +30,11 @@ function questionTwo() {
   console.log('jaeRealPersonQuestion' + jaeRealPersonQuestion);
 
   if (jaeRealPersonQuestion === 'y' || jaeRealPersonQuestion === 'Y' || jaeRealPersonQuestion === 'YES' || jaeRealPersonQuestion === 'Yes' || jaeRealPersonQuestion === 'yes') {
-
     alert('Wrong Answer!! ' + whatIsYourName + ' Jae got laid off due to Covid-19!');
+    totalIncorrect++;
   } else if (jaeRealPersonQuestion === 'n' || jaeRealPersonQuestion === 'N' || jaeRealPersonQuestion === 'NO' || jaeRealPersonQuestion === 'No' || jaeRealPersonQuestion === 'no') {
-
+    
+    totalCorrect++;
     alert('You are correct!! ' + whatIsYourName, + ' Jae is not working, but actively looking!!');
   }
   console.log('finished my second function')
@@ -49,9 +53,10 @@ function questionThree() {
 
   if (jaeRealPersonAnswer === 'y' || jaeRealPersonAnswer === 'Y' || jaeRealPersonAnswer === 'YES' || jaeRealPersonAnswer === 'Yes' || jaeRealPersonAnswer === 'yes') {
     alert('Of Course!!. ' + whatIsYourName + ' You should give Jae a chance, he might suprise you!!');
-
+    totalCorrect++;
   } else if (jaeRealPersonAnswer === 'n' || jaeRealPersonAnswer === 'N' || jaeRealPersonAnswer === 'NO' || jaeRealPersonAnswer === 'No' || jaeRealPersonAnswer === 'no') {
     alert('That is unfortunate!!. ' + whatIsYourName + ' Jae is a worth taking a chance, He is an AWESOME PERSON!!');
+    totalIncorrect++;
   }
 }
 questionThree()
@@ -61,9 +66,10 @@ function questionFour() {
 
   if (jaeTravel === 'y' || jaeTravel === 'Y' || jaeTravel === 'YES' || jaeTravel === 'Yes' || jaeTravel === 'yes') {
     alert('Heck Yeah, ' + whatIsYourName + ' Jae is ex-military, practice martial artist. Jae is discplined, has intergrity, can work under pressure, and a highly motivated indivdual!!'); 
-
+    totalCorrect++;
   } else if (jaeTravel === 'n' || jaeTravel === 'N' || jaeTravel === 'NO' || jaeTravel === 'No' || jaeTravel === 'no') {
     alert('Seriously!!. ' + whatIsYourName +  ' you should not judge a book by his cover until you read a few chapters!!');
+    totalIncorrect++;
   }
 }
 questionFour()
@@ -74,9 +80,10 @@ function questionFive() {
 
   if (jaeHobbieQuestion === 'y' || jaeHobbieQuestion === 'Y' || jaeHobbieQuestion === 'YES' || jaeHobbieQuestion === 'Yes' || jaeHobbieQuestion === 'yes') {
     alert('Absolutely ' + whatIsYourName +  ' You can contact Jae anytime!!');
-
+    totalCorrect++;
   }  else if (jaeHobbieQuestion === 'n' || jaeHobbieQuestion === 'N' || jaeHobbieQuestion === 'NO' || jaeHobbieQuestion === 'no') {
       alert('I was just joking around!!. ' + whatIsYourName + ' please do so!!');
+    totalIncorrect++;
   }
     console.log('jaeHobbieQuestion' + jaeHobbieQuestion);
 }
@@ -89,16 +96,21 @@ function myQuestion() {
     jaeAgeAnswer = parseInt(jaeAgeAnswer)
     if (jaeAgeAnswer <= 28) {
       alert('too low!!')
+      totalIncorrect++;
     }
+    totalCorrect++;
     if (jaeAgeAnswer >= 30) {
       alert('too high')
+      totalIncorrect++;
     }
     if (jaeAgeAnswer === 29) {
       alert('GREAT JOB, You will go along way Grasshopper!');
+      totalCorrect++;
       break;
     }
     if (i === 3) {
       alert('No more guessing, Jae got out at age 29 LOL')
+     
     }
   }
 }
@@ -109,16 +121,18 @@ myQuestion()
 //  console.log(i)
 
 function questionSix() {
+
   for (var i = 0; i < 6; i++) {
     var jaeMulitpleQuestion = prompt('Do you think Jae will find work soon??');
-    var j = ['maybe', 'no', 'no way', 'who knows', 'yes', 'not sure' ];
+    var j = ['maybe', 'no', 'no way', 'who knows', 'nope', 'not sure' ];
 
-    if (jaeMulitpleQuestion === j[0] || jaeMulitpleQuestion === j[4]) {
+    if (jaeMulitpleQuestion === j[0, 3] || jaeMulitpleQuestion === j[5]) {
       console.log('maybe')
-      alert('you got it right! ' + whatIsYourName + ' your final score ' + 'you score');
+      alert('you got it right! ' + whatIsYourName + ' your final score for this game is ' + totalCorrect +  'correct answers from the game you played today');
+      
     }
-    if (jaeMulitpleQuestion !== j[0] || jaeMulitpleQuestion !== j[4]) {
-      alert('you got it wrong! ' + whatIsYourName + ' your final score ' + 'you score');
+    if (jaeMulitpleQuestion !== j[1, 2] || jaeMulitpleQuestion !== j[4]) {
+      alert('you got it wrong! ' + whatIsYourName + ' your final score for this game is ' + totalIncorrect + ' in-correct answers from the game you played today');
     } 
       break;  
   }
@@ -128,79 +142,5 @@ function questionSix() {
 
 }
 questionSix();
-
-
-
-
-
-
-
-/*
-// Instructions
-// Continue to build off of your lab 02 by adding additonal functionality to your About Me Guessing Game. Work in the same repository as you did for Lab 2, and modify your README.md file to describe your project with the new features you are adding.
-
-// As a user, I would like to view a series of data related to the site owners interest so that the I can quickly view more information about them.
-// Create a “Top Ten” at the bottom of your HTML page as an ordered list in HTML. Some ideas that you can include could be top ten movies, top ten favorite places, or top ten places to visit. You may choose to do whatever top ten list that you wish.
-// Convert your work experience and education summary into an unordered list using HTML
-// As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
-// Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
-// Indicates through an alert if the guess is “too high” or “too low”.
-// It should give the user exactly four opportunities to get the correct answer.
-// After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
-// As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
-// Add a 7th question that has multiple possible correct answers that are stored in an array.
-// Give the user 6 attempts to guess the correct answer.
-// The guesses will end once the user guesses a correct answer or they run out of attempts.
-// Display all the possible correct answers to the user.
-// Consider using a loop of some sort for this question.
-// As a user, I would like to know my final score so that I can know how well I did.
-// Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
-// In addition to the user stories stated above, continue to use console.logs throughout the code to track the questions that are being asked to the user, and add addtional CSS to style your HTML page.
-
-// Stay within scope of what was taught so far in class. Do not write functions within your application. Avoid the use of “helper” methods such as .includes() and .join(). Work with the instructional staff directly if you have questions about this requirement.
-
-// Stretch Goals
-// After completing all of the requirements listed above, you may optionally challenge yourself further by implementing some or all of the following into your application.
-
-// Attempt to make your code more DRY by putting all of the questions, answers, and responses to the first five yes/no questions into arrays (or even one huge multidimensional array), and modifying the game logic such that a ‘for’ loop will control the flow from question to question.
-// Make the number-guessing question use a random number as its correct answer.
-// Resources
-// Utulize the same resources as described in Lab 02.
-
-// Submission Instructions
-// Submit the link to your GitHub repo for this project
-// Add a comment to this Canvas submission with answers to the following questions
-// How did this go, overall?
-// What observations or questions do you have about what you’ve learned so far?
-// How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
-
-
-
-
-
-
-
-
-
-
-// switch(jaeAgeAnswer){
-//     case 'two':
-//       alert('heck yeah');
-//     case 'zero':
-//       alert('hes definitely older');
-//       break;
-//     case 'one':
-//       alert('almost there');
-//       break;
-//       case 'three':
-//       alert('amost there');
-//       break;
-//     default:
-//       alert('not even in the ballpark');
-// }
-
-
-
-*/
 
 
